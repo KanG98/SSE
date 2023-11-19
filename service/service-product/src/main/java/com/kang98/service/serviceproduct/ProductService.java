e711bb6b750864f7acd0649991de9e5e8d1cd63a
KanG98 <yankang198@gmail.com>
2023-11-19 04:30:59 -0500

    set up basics

---
+++ service/service-product/src/main/java/com/kang98/service/serviceproduct/ProductService.java
@@ -0,0 +1,12 @@
+package com.kang98.service.serviceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductService.class, args);
+    }
+}
