8b879d97cd056f33adc9875c9392bb061904d1fd
KanG98 <yankang198@gmail.com>
2023-12-26 13:02:06 -0500

    modify config for auth service test

--- service/service-auth/src/test/java/com/kang98/service/serviceauth/config/TestSecurityConfig.java
+++ service/service-auth/src/test/java/com/kang98/service/serviceauth/config/TestSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.kang98.service.serviceauth.config;
 
 import com.kang98.service.serviceauth.service.helpers.JwtHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 @EnableWebSecurity
@@ -19,5 +24,18 @@ public class TestSecurityConfig {
     @Bean
     public JwtHelper jwtService() {
         return new JwtHelper(secret);
+    }
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("app.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("app.datasource.url"));
+        dataSource.setUsername(env.getProperty("app.datasource.username"));
+        dataSource.setPassword(env.getProperty("app.datasource.password"));
+        return dataSource;
     }
 }
