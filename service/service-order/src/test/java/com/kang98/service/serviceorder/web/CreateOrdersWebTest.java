89b904c1cb18056d2d46c1e313560e12a3d75c54
KanG98 <yankang198@gmail.com>
2023-12-28 10:12:19 -0500

    add order service web test

--- service/service-order/src/test/java/com/kang98/service/serviceorder/web/CreateOrdersWebTest.java
+++ service/service-order/src/test/java/com/kang98/service/serviceorder/web/CreateOrdersWebTest.java
@@ -1,17 +1,24 @@
 package com.kang98.service.serviceorder.web;
 
+import com.kang98.service.serviceauth.dto.AuthRequest;
+import com.kang98.service.serviceauth.dto.AuthResponse;
 import com.kang98.service.serviceorder.config.OrderServiceTestConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 
 import static com.kang98.service.serviceorder.config.OrderServiceTestConfig.TEST_CREATE_ORDER_INVALID_REQUEST;
@@ -27,6 +34,28 @@ public class CreateOrdersWebTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Value("${JWTUSER:sampleUser}")
+    private String username;
+
+    @Value("${JWTPWD:samplePwd}")
+    private String password;
+
+    String getToken() throws URISyntaxException {
+        System.out.println("username" + username);
+        System.out.println("pwd" + password);
+        String token = "";
+        RestTemplate restTemplate = new RestTemplate();
+        if (!username.equals("sampleUser") && !password.equals("samplePwd")) {
+            final String baseUrl = "http://localhost:" + "50231" + "/authenticate";
+
+            AuthRequest request = AuthRequest.builder().username(username).password(password).build();
+            URI location = new URI(baseUrl);
+            ResponseEntity<AuthResponse> response =
+                    restTemplate.postForEntity(location, request, AuthResponse.class);
+            token = response.getBody().getJwt_token();
+        }
+        return token;
+    }
 
     @Test
     void testCreateOrderHttpPost_expectedSuccess() throws Exception {
@@ -34,6 +63,7 @@ public class CreateOrdersWebTest {
         String JSON_STRING= new String(requestJson.readAllBytes(), StandardCharsets.UTF_8);
 
         mockMvc.perform(post("/orders")
+                        .header("authorization", "Bearer " + getToken())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JSON_STRING)
                 )
@@ -46,6 +76,7 @@ public class CreateOrdersWebTest {
         String JSON_STRING= new String(requestJson.readAllBytes(), StandardCharsets.UTF_8);
 
         mockMvc.perform(post("/orders")
+                        .header("authorization", "Bearer " + getToken())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JSON_STRING)
                 )
