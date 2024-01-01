e101f909be9eccf5975622204a7866df94528d92
KanG98 <yankang198@gmail.com>
2024-01-01 12:46:07 -0500

    add auth to deliveryman service

--- service/service-deliveryman/src/main/java/com/kang98/service/servicedeliveryman/DeliverymanServiceApplication.java
+++ service/service-deliveryman/src/main/java/com/kang98/service/servicedeliveryman/DeliverymanServiceApplication.java
@@ -2,13 +2,10 @@ package com.kang98.service.servicedeliveryman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.kang98.data.dataorder.repository")
-
 public class DeliverymanServiceApplication {
 
     public static void main(String[] args) {
