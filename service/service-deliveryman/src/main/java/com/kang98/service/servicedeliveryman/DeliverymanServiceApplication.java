bff424c6c40c1e6dae3e3b4c0ab1f7de8ed69fcf
KanG98 <yankang198@gmail.com>
2024-01-04 12:42:06 -0500

    fix jpa config for dm service

--- service/service-deliveryman/src/main/java/com/kang98/service/servicedeliveryman/DeliverymanServiceApplication.java
+++ service/service-deliveryman/src/main/java/com/kang98/service/servicedeliveryman/DeliverymanServiceApplication.java
@@ -2,10 +2,12 @@ package com.kang98.service.servicedeliveryman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.kang98.data.datadeliveryman.repository")
+@EnableJpaRepositories("com.kang98.data.dataauth.repository")
 public class DeliverymanServiceApplication {
 
     public static void main(String[] args) {
